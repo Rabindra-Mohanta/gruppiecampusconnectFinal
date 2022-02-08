d139c68a456939b27c153e848016db9ee5af8a0a
Frenzin07 <frenzindev01@gmail.com>
2022-02-08 10:49:02 +0530

    add ticket model

---
+++ app/src/main/java/school/campusconnect/datamodel/add_tikit/AddTicketRequest.java
@@ -0,0 +1,26 @@
+package school.campusconnect.datamodel.add_tikit;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class AddTicketRequest implements Serializable {
+    public String text;
+    public ArrayList<String> fileName;
+    public String fileType;// pdf/youtube/image
+
+    public LocationData location;
+
+    public static class LocationData implements Serializable
+    {
+        public String latitude;
+        public String longitude;
+        public String address;
+        public String landmark;
+        public String pincode;
+    }
+
+    public static class AddTicketRes implements Serializable
+    {
+
+    }
+}
